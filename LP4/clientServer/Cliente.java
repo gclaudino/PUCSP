0e8601ed0c49632c23106794e5787fab8cb2fdcf
Gabriel Claudino Rabelo Silva <ra00178262@D-090865.pucsp.br>
2018-05-29 12:31:15 -0300

    On branch master  Your branch is up-to-date with 'origin/master'.
     Changes to be committed:
    	modified:   Cliente.class
    	modified:   Cliente.java
    	modified:   package.bluej
    
     Changes not staged for commit:
    	deleted:    __SHELL3.class
    	deleted:    __SHELL3.java

--- LP4/clientServer/Cliente.java
+++ LP4/clientServer/Cliente.java
@@ -6,7 +6,7 @@ class Cliente {
 
     public void run() throws IOException {
         System.out.println("Conectando com o servidor de notas LP4...");
-        try (Socket servidor = new Socket("localhost", 9001)) {
+        try (Socket servidor = new Socket("172.17.31.8", 9001)) {
             TarefaCliente t = new TarefaCliente(servidor);
             t.run();
         } // servidor.close()
